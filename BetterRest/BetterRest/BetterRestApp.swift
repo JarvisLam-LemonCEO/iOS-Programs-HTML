059272ef436a7bf8d1ddab9a2c6cfaa9b0a608a2
JarvisLam-LemonCEO <102021121+JarvisLam-LemonCEO@users.noreply.github.com>
2022-03-21 01:34:22 -0700

    Add files via upload

---
+++ BetterRest/BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Jarvis Lam on 1/4/22.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
