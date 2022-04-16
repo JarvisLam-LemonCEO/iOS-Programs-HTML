2817754153a9a6057c8cba2dd899c53181480b40
Jarvis Lam <102021121+JarvisLam-LemonCEO@users.noreply.github.com>
2022-04-16 00:16:17 -0700

    Add files via upload

---
+++ IntToRoman.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+  
+using namespace std; 
+string integer_to_Roman(int n) {
+  string str_romans[] = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+  int values[] = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+  string result = "";
+  
+  for ( int i = 0; i < 13; ++i){
+    while(n - values[i] >= 0){
+      result += str_romans[i];
+      n -= values[i];
+    }
+  }
+  return result;
+}
+
+int main() {  
+  int n = 7;
+  cout << "Enter an integer: ";
+  cin >> n;
+  cout << "Integer " << n << " : Roman " << integer_to_Roman(n) << endl;
+}
