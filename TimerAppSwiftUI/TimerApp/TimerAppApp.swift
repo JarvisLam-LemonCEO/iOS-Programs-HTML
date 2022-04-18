df85e00bf6116866c5956dffe30b22dbd4df550e
Jarvis Lam <102021121+JarvisLam-LemonCEO@users.noreply.github.com>
2022-04-18 12:41:55 -0700

    Add files via upload

---
+++ TimerAppSwiftUI/TimerApp/TimerAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimerAppApp.swift
+//  TimerApp
+//
+//  Created by Jarvis Lam on 4/18/22.
+//
+
+import SwiftUI
+
+@main
+struct TimerAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
